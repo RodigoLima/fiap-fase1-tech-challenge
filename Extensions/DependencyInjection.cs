1e57a248d2d439483dbaa36a5c60584e92abba06
Rodrigo <ferreirarodrigo230@gmail.com>
2025-04-06 16:34:32 -0400

    Refactor user management architecture and services
    - Introduced interfaces and services for user management.
    - Updated `Program.cs` to add application services and configure DbContext.
    - Implemented `IUserRepository` in `UserRepository` with CRUD methods.
    - Created `UserController` to handle user-related HTTP requests.
    - Defined `IUserService` and implemented `UserService` for business logic.
    - Cleaned up project file by removing unnecessary folder references.
    - Organized dependency injection in a new `DependencyInjection` class.
    - Enhanced separation of concerns for improved maintainability and testability.

---
+++ Extensions/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using fiap_fase1_tech_challenge.Repositories;
+using fiap_fase1_tech_challenge.Services;
+
+namespace fiap_fase1_tech_challenge.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
+
+            // Adicione outras dependências aqui...
+
+            return services;
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using fiap_fase1_tech_challenge.Models;
+using fiap_fase1_tech_challenge.Repositories;
+
+namespace fiap_fase1_tech_challenge.Services
+{
+    public class UserService: IUserService
+    {
+
+        private readonly IUserRepository _repository;
+
+        public UserService(IUserRepository respository)
+        {
+            _repository = respository;
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync() => _repository.GetAllAsync();
+        public Task<User?> GetByIdAsync(Guid id) => _repository.GetByIdAsync(id);
+        public Task<User> CreateAsync(User user) => _repository.CreateAsync(user);
+        public Task<bool> UpdateAsync(User user) => _repository.UpdateAsync(user);
+        public Task<bool> DeleteAsync(Guid id) => _repository.DeleteAsync(id);
+
+    }
+}
