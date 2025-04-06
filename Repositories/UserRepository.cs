16e2ede259c9ba8f563ad12783c1aef05d336e9f
Rodrigo <ferreirarodrigo230@gmail.com>
2025-04-06 14:14:21 -0400

    Reintroduce ApplicationContext and update migrations
    This commit reintroduces the `ApplicationContext` class, which inherits from `DbContext` and includes `DbSet` properties for `User`, `Role`, `Game`, `GameLibrary`, and `Promotion`. It also adds methods for seeding initial data.
    
    Migration files are created or updated to define the structure of the `Users`, `Roles`, `Games`, `GameLibraries`, and `Promotions` tables, including primary keys and foreign key relationships. Initial data for roles and an admin user is seeded to ensure a predefined starting state.
    
    Additionally, the `ContextModelSnapshot` class is updated to reflect the current model state, and a new `UserRepository` class is introduced for user data access.

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using fiap_fase1_tech_challenge.Database;
+using fiap_fase1_tech_challenge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace fiap_fase1_tech_challenge.Repositories
+{
+    public class UserRepository
+    {
+
+        private readonly ApplicationContext _context;
+
+
+        public UserRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
