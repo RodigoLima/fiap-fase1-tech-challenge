0e0f9ee0622a0e85e239a15190f9a2b2fdcfc481
Renato Müller Ventura <re.infotec@gmail.com>
2025-05-17 15:47:51 -0300

    feat:Adds Game service and prevents duplicate games
    Introduces a new service layer for managing game-related operations,
    including creation, retrieval, update, and deletion.
    
    Prevents the creation of duplicate games by checking for existing
    games with the same name before adding a new entry to the database.

---
+++ Services/GameService.cs
@@ -0,0 +1,71 @@
+﻿using fiap_fase1_tech_challenge.Database;
+using fiap_fase1_tech_challenge.DTOs.Game;
+using fiap_fase1_tech_challenge.Enums;
+using fiap_fase1_tech_challenge.Models;
+using fiap_fase1_tech_challenge.Repositories;
+using fiap_fase1_tech_challenge.Repositories.Interfaces;
+using fiap_fase1_tech_challenge.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Authentication;
+
+namespace fiap_fase1_tech_challenge.Services
+{
+    public class GameService : IGameService
+    {
+
+        private readonly IGameRepository _gameRepository;
+        private readonly IRoleRepository _roleRepository;
+
+        public GameService(IGameRepository GameRepository, IRoleRepository roleRepository)
+        {
+            _gameRepository = GameRepository;
+            _roleRepository = roleRepository;
+        }
+
+        public Task<IEnumerable<Game>> GetAllAsync() => _gameRepository.GetAllAsync();
+        public Task<Game?> GetByIdAsync(int id) => _gameRepository.GetByIdAsync(id);
+        public async Task<GameResponse> CreateAsync(GameCreateRequest request)
+        {
+            var newGame = new Game
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Price = request.Price,
+                ReleasedDate = request.ReleasedDate,
+                Genre = request.Genre
+            };
+
+            var createdGame = await _gameRepository.CreateAsync(newGame);
+
+            return new GameResponse
+            {
+                Id = createdGame.Id,
+                Name = createdGame.Name,
+                Description = createdGame.Description,
+                Price = createdGame.Price,
+                ReleasedDate = createdGame.ReleasedDate,
+                Genre = createdGame.Genre
+            };
+        }
+        public async Task<bool> UpdateAsync(int id, GameUpdateRequest game)
+        {
+            var existingGame = await _gameRepository.GetByIdAsync(id);
+            if (existingGame == null)
+                return false;
+
+            existingGame.Name = game.Name;
+            existingGame.Description = game.Description;
+            existingGame.Price = game.Price;
+            if (game.ReleasedDate != null)
+                existingGame.ReleasedDate = game.ReleasedDate;
+            existingGame.Genre = game.Genre;
+
+            await _gameRepository.UpdateAsync(existingGame);
+
+            return true;
+        }
+        public Task<bool> DeleteAsync(int id) => _gameRepository.DeleteAsync(id);
+    }
+
+
+}
